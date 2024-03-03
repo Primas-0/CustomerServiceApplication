b7cfd78cfddffbcb9fe3a914dad42ade3526f6f7
Primas <136030538+Primas-0@users.noreply.github.com>
2024-03-02 20:36:48 -0500

    segfault fixed

--- fastq.cpp
+++ fastq.cpp
@@ -1,13 +1,13 @@
 // CMSC 341 - Spring 2024 - Project 1
 #include "fastq.h"
 
-ArrayBuffer::ArrayBuffer(int capacity){
+ArrayBuffer::ArrayBuffer(int capacity) {
     //allocate memory for array if capacity >= 1
     if (capacity < 1) {
         m_buffer = nullptr;
         m_capacity = 0;
     } else {
-        m_buffer = new int [capacity];
+        m_buffer = new int[capacity];
         m_capacity = capacity;
     }
     //initialize all other member variables
@@ -17,7 +17,7 @@ ArrayBuffer::ArrayBuffer(int capacity){
     m_next = nullptr;
 }
 
-void ArrayBuffer::clear(){
+void ArrayBuffer::clear() {
     //deallocate all memory
     delete[] m_buffer;
 
@@ -29,7 +29,7 @@ void ArrayBuffer::clear(){
     m_next = nullptr;
 }
 
-ArrayBuffer::~ArrayBuffer(){
+ArrayBuffer::~ArrayBuffer() {
     ArrayBuffer::clear();
 }
 
@@ -41,7 +41,7 @@ bool ArrayBuffer::empty() const {
     return false;
 }
 
-void ArrayBuffer::enqueue(int data){
+void ArrayBuffer::enqueue(int data) {
     if (m_count == m_capacity) {
         throw overflow_error("Buffer is full");
     }
@@ -54,7 +54,7 @@ void ArrayBuffer::enqueue(int data){
     m_count++;
 }
 
-int ArrayBuffer::dequeue(){
+int ArrayBuffer::dequeue() {
     if (m_count == 0) {
         throw underflow_error("Buffer is empty");
     }
@@ -72,7 +72,7 @@ int ArrayBuffer::dequeue(){
     return dequeuedData;
 }
 
-ArrayBuffer::ArrayBuffer(const ArrayBuffer & rhs){
+ArrayBuffer::ArrayBuffer(const ArrayBuffer &rhs) {
     //construct current object with same dimensions as rhs
     m_capacity = rhs.m_capacity;
     m_count = rhs.m_count;
@@ -87,7 +87,7 @@ ArrayBuffer::ArrayBuffer(const ArrayBuffer & rhs){
     }
 }
 
-const ArrayBuffer & ArrayBuffer::operator=(const ArrayBuffer & rhs){
+const ArrayBuffer &ArrayBuffer::operator=(const ArrayBuffer &rhs) {
     //if self-assignment, return current object without any changes
     if (this == &rhs) {
         return *this;
@@ -112,23 +112,22 @@ const ArrayBuffer & ArrayBuffer::operator=(const ArrayBuffer & rhs){
     return *this;
 }
 
-void ArrayBuffer::dump(){
+void ArrayBuffer::dump() {
     int start = m_start;
     //int end = m_end;
     int counter = 0;
     cout << "ArrayBuffer size: " << m_capacity << " : ";
-    if (!empty()){
-        while(counter < m_count){
+    if (!empty()) {
+        while (counter < m_count) {
             cout << m_buffer[start] << "[" << start << "]" << " ";
             start = (start + 1) % m_capacity;
             counter++;
         }
         cout << endl;
-    }
-    else cout << "ArrayBuffer is empty!" << endl;
+    } else cout << "ArrayBuffer is empty!" << endl;
 }
 
-ListBuffer::ListBuffer(int minBufCapacity){
+ListBuffer::ListBuffer(int minBufCapacity) {
     //create ListBuffer object which contains the first ArrayBuffer object
     if (minBufCapacity < 1) {
         m_cursor = new ArrayBuffer(DEFAULT_MIN_CAPACITY);
@@ -136,20 +135,23 @@ ListBuffer::ListBuffer(int minBufCapacity){
         m_cursor = new ArrayBuffer(minBufCapacity);
     }
 
+    //make sure linked list is always circular
+    m_cursor->m_next = m_cursor;
+
     //update size to reflect new node
     m_listSize = 1;
 }
 
-ListBuffer::~ListBuffer(){
+ListBuffer::~ListBuffer() {
     ListBuffer::clear();
 }
 
-void ListBuffer::clear(){
+void ListBuffer::clear() {
     ArrayBuffer *curr = m_cursor;
 
     //loop through the linked list and remove each array buffer, until it circles back to the beginning
     do {
-        ArrayBuffer* next = curr->m_next;
+        ArrayBuffer *next = curr->m_next;
         curr->ArrayBuffer::clear();
         curr = next;
     } while (curr != m_cursor);
@@ -159,11 +161,11 @@ void ListBuffer::clear(){
     m_listSize = 0;
 }
 
-void ListBuffer::enqueue(const int& data) {
+void ListBuffer::enqueue(const int &data) {
     try {
         m_cursor->enqueue(data);
-    } catch (overflow_error e) {
-        ArrayBuffer* newBuffer = nullptr;
+    } catch (overflow_error &e) {
+        ArrayBuffer *newBuffer = nullptr;
 
         //once the buffer is full, insert a new node (ArrayBuffer object) into the linked list
         if (m_cursor->m_capacity < MAX_FACTOR * m_minBufCapacity) {
@@ -189,7 +191,7 @@ int ListBuffer::dequeue() {
 
     try {
         return oldestBuffer->dequeue();
-    } catch (underflow_error e) {
+    } catch (underflow_error &e) {
         //if there is only one node in the linked list and its buffer is empty, do not remove the node (throw exception)
         if (m_listSize == 1) {
             throw underflow_error("Must have at least one node in the linked list");
@@ -197,16 +199,16 @@ int ListBuffer::dequeue() {
 
         //remove the node containing the empty buffer from the linked list
         m_cursor->m_next = oldestBuffer->m_next;
-        oldestBuffer->ArrayBuffer::clear();
+        delete oldestBuffer;
         m_listSize--;
 
         //remove data from the next node
-        oldestBuffer = oldestBuffer->m_next;
+        oldestBuffer = m_cursor->m_next;
         return oldestBuffer->dequeue();
     }
 }
 
-ListBuffer::ListBuffer(const ListBuffer & rhs){
+ListBuffer::ListBuffer(const ListBuffer &rhs) {
     //mirror member variables
     m_listSize = rhs.m_listSize;
     m_minBufCapacity = rhs.m_minBufCapacity;
@@ -236,7 +238,7 @@ ListBuffer::ListBuffer(const ListBuffer & rhs){
     prevBuffer->m_next = m_cursor;
 }
 
-const ListBuffer & ListBuffer::operator=(const ListBuffer & rhs){
+const ListBuffer &ListBuffer::operator=(const ListBuffer &rhs) {
     //if self-assignment, return current object without any changes
     if (this == &rhs) {
         return *this;
@@ -265,9 +267,9 @@ const ListBuffer & ListBuffer::operator=(const ListBuffer & rhs){
     return *this;
 }
 
-void ListBuffer::dump(){
-    ArrayBuffer* temp = m_cursor->m_next;
-    for(int i=0;i<m_listSize;i++){
+void ListBuffer::dump() {
+    ArrayBuffer *temp = m_cursor->m_next;
+    for (int i = 0; i < m_listSize; i++) {
         temp->dump();
         temp = temp->m_next;
         cout << endl;
